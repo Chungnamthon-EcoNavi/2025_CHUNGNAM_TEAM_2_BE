b3a3d58f8a7f7a8c043d4f6edccf984dcee1fffc
KTH <pni2396@gmail.com>
2025-07-18 20:42:06 +0900

    setting: spring-boot project init

---
+++ econavi-backend/econavi/src/test/java/com/example/econavi/EconaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.econavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconaviApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
