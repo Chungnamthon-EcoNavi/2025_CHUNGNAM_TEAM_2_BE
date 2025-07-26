c8895708b646b653284c6a018fbc4858960188f7
KTH <pni2396@gmail.com>
2025-07-26 22:42:13 +0900

    chore: Test용 DDL 주석 추가
    - 로컬 DB에서 테스트 필요 시 application.yml의 profiles.active: dev 주석 해제
    - 로컬 DB에 해당 DDL 실행

--- econavi/src/main/resources/schema.sql
+++ econavi/src/main/resources/schema.sql
@@ -1,3 +1,9 @@
+-- Test용 DB 및 User 생성 쿼리
+-- CREATE DATABASE econavi_test;
+-- CREATE USER 'econavi_test'@'%' IDENTIFIED BY 'econaviTest';
+-- GRANT ALL PRIVILEGES ON econavi_test.* TO 'econavi_test'@'%';
+-- USE econavi_test;
+
 CREATE TABLE IF NOT EXISTS members (
     id
     BIGINT
