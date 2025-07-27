64df3a050a1d548a6b6ab768de7f2e8d84ef7e5f
KTH <pni2396@gmail.com>
2025-07-28 08:19:37 +0900

    test: test 코드 수정
    - AuthIntegrationTest 추가
    - BookmarkIntegrationTest 프로필 선언
    - Swagger 설명 명시

--- econavi/src/main/java/com/example/econavi/common/controller/FileController.java
+++ econavi/src/main/java/com/example/econavi/common/controller/FileController.java
@@ -1,6 +1,9 @@
 package com.example.econavi.common.controller;
 
 import com.example.econavi.common.service.FileStorageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
@@ -13,9 +16,15 @@ import java.net.MalformedURLException;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "정적 리소스 관련 서비스", description = "이미지, 파일 등 다운로드")
 public class FileController {
     private final FileStorageService fileStorageService;
 
+    @Operation(
+            summary = "",
+            description = "정적 리소스 url을 통해 다운로드",
+            security = @SecurityRequirement(name = "bearerAuth")
+    )
     @GetMapping("/download")
     public ResponseEntity<Resource> download(
             @RequestParam String file_name
