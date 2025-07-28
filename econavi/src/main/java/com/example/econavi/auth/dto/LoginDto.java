34f7ee7739dc4b7eac80583e2ea427f71ea290d3
KTH <pni2396@gmail.com>
2025-07-28 14:14:47 +0900

    fix: 인증 시 validation 수정
    - 비밀번호 평문 8~72자리로 제한
    - 로그인 시 email 패턴 검증 수정

--- econavi/src/main/java/com/example/econavi/auth/dto/LoginDto.java
+++ econavi/src/main/java/com/example/econavi/auth/dto/LoginDto.java
@@ -1,7 +1,7 @@
 package com.example.econavi.auth.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,7 +14,7 @@ public class LoginDto {
     @Builder
     public static class Request{
         @NotBlank
-        @Email(message = "Invalid email pattern")
+        @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,100}$", message = "계정은 이메일 형식이어야 합니다.")
         private String username;
         @NotBlank
         private String password;
