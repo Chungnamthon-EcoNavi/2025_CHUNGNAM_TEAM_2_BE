6a936d11e467d8057ff18a553f08eb838e2f1aed
KTH <pni2396@gmail.com>
2025-07-29 04:14:08 +0900

    feat: 사진 관련 API 구현 및 통합테스트
    - 사용자 프로필 사진, 장소 사진, 상품 사진 업로드 및 메타데이터 조회 API 구현
    - 리소스 다운로드 통합 테스트
    - 다수의 사진 업로드 및 조회 가능
    - 상품 사진은 Role.STAFF에게만 허용됨

--- econavi/src/main/java/com/example/econavi/photo/repository/PlacePhotoRepository.java
+++ econavi/src/main/java/com/example/econavi/photo/repository/PlacePhotoRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface PlacePhotoRepository extends JpaRepository<PlacePhoto, Long> {
     List<PlacePhoto> findByPlace(Place place);
+
+    List<PlacePhoto> findByPlace_id(Long placeId);
 }
